9ddbc8ec377500f382948faf6e72def50d3df61c
ciphermonk <ciphermonk@altera-vita.com>
2013-03-31 22:02:08 +0800

    Creating network communication pipeline and send Version message

--- src/Bitcoin/Types.hs
+++ src/Bitcoin/Types.hs
@@ -3,6 +3,9 @@ module Bitcoin.Types
 , VarString(..)
 , NetworkAddress(..) 
 , Version(..)
+, MessageHeader(..)
+, encodeMessage
+, readMessage
 ) where
 
 import Data.Word
@@ -10,11 +13,16 @@ import Data.Binary
 import Data.Binary.Get
 import Data.Binary.Put
 
+import Data.Char --for ord
+
 import Control.Applicative
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 
-newtype VarInt = VarInt Word64
-    deriving (Eq, Ord, Num, Real, Enum, Integral, Show, Read)
+import qualified Crypto.Hash.SHA256 as H
+
+newtype VarInt = VarInt { getVarInt :: Word64 }
+    deriving (Eq, Ord, Bounded, Show, Read)
     
 instance Binary VarInt where
 
@@ -38,37 +46,35 @@ instance Binary VarInt where
             putWord64le $ fromIntegral x
         | otherwise = error "VarInt cannot be larger that 8 bytes"
 
-newtype VarString = VarString BL.ByteString
+newtype VarString = VarString BS.ByteString
     deriving (Eq, Show, Read)
 
 instance Binary VarString where
     
     get = do
-        size <- (get :: Get VarInt)
-        VarString <$> getLazyByteString (fromIntegral size)
+        (VarInt size) <- (get :: Get VarInt)
+        VarString <$> getByteString (fromIntegral size)
         
     put (VarString x) = do
-        put . VarInt . fromIntegral . BL.length $ x
-        putLazyByteString x
+        put . VarInt . fromIntegral . BS.length $ x
+        putByteString x
 
 data NetworkAddress = NetworkAddress {
     naServices :: Word64,
-    naAddress  :: BL.ByteString,
+    naAddress  :: BS.ByteString,
     naPort     :: Word16
 } deriving (Eq, Show, Read)
 
 instance Binary NetworkAddress where
 
-    get = do
-        services <- getWord64le
-        address  <- getLazyByteString 16
-        port     <- getWord16be 
-        return $ NetworkAddress services address port
+    get = NetworkAddress <$> getWord64le
+                         <*> getByteString 16
+                         <*> getWord16be 
 
-    put (NetworkAddress services address port) = do
-        putWord64le services
-        putLazyByteString address
-        putWord16be port
+    put (NetworkAddress s a p) = do
+        putWord64le       s
+        putByteString     a
+        putWord16be       p
 
 data Version = Version {
     vVersion     :: Word32,
@@ -77,9 +83,13 @@ data Version = Version {
     vAddrRecv    :: NetworkAddress,
     vAddrSend    :: NetworkAddress,
     vNonce       :: Word64,
+    vUserAgent   :: VarString,
     vStartHeight :: Word32,
     vRelay       :: Bool
 } deriving (Show, Read)
+
+class Binary a => ProtocolMessage a where
+    command :: a -> String
 
 instance Binary Version where
 
@@ -89,16 +99,63 @@ instance Binary Version where
                   <*> get
                   <*> get
                   <*> getWord64le
+                  <*> get
                   <*> getWord32le
                   <*> get
 
-    put (Version v s t ar as n sh r) = do
+    put (Version v s t ar as n ua sh r) = do
         putWord32le v
         putWord64le s
         putWord64le t
         put         ar
         put         as
         putWord64le n
+        put         ua
         putWord32le sh
         put         r
+
+instance ProtocolMessage Version where
+    command _ = "version" ++ (take 5 $ repeat '\NUL')
+
+data MessageHeader = MessageHeader {
+    mhMagic    :: BS.ByteString,
+    mhCommand  :: BS.ByteString,
+    mhLength   :: Word32,
+    mhChecksum :: BS.ByteString
+} deriving (Show, Read)
+
+instance Binary MessageHeader where
+
+    get = MessageHeader <$> getByteString 4
+                        <*> getByteString 12
+                        <*> getWord32le
+                        <*> getByteString 4
+
+    put (MessageHeader m c l cs) = do
+        putByteString m
+        putByteString c
+        putWord32le l
+        putByteString cs
+
+doubleSHA256 :: BS.ByteString -> BS.ByteString
+doubleSHA256 = H.hash . H.hash
+
+checksum :: BS.ByteString -> BS.ByteString
+checksum = (BS.take 4) . doubleSHA256
+
+toStrict :: BL.ByteString -> BS.ByteString
+toStrict = BS.concat . BL.toChunks
+
+encodeMessage :: ProtocolMessage a => a -> BS.ByteString
+encodeMessage x = let payload = toStrict . encode $ x
+                      cmd = BS.pack $ map (fromIntegral . ord) (command x)
+                      magic = BS.pack [11,17,9,7]
+                      size = fromIntegral $ BS.length payload
+                      chksum = checksum payload
+                      header = toStrict . encode $ 
+                          MessageHeader magic cmd size chksum
+                      in header `BS.append` payload
+
+readMessage :: BL.ByteString -> MessageHeader
+readMessage payload = decode payload
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,32 @@
+import Network
+import System.IO
 import Data.Binary
+import Data.Char --for ord
+import Numeric -- for showHex
+import System.Random -- for randon nonce
+
+import Data.Time.Clock.POSIX -- unix time
+
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 
-import Bitcoin.Types
+import qualified Bitcoin.Types as B
 
 zeroAddr = [0,0,0,0,0,0,0,0,0,0,255,255,0,0,0,0]
-addr = NetworkAddress 1 (BL.pack zeroAddr) 0
-version = Version 1 1 0 addr addr 0 0 False
+addr = B.NetworkAddress 1 (BS.pack zeroAddr) 0
+userAgent = B.VarString $ BS.pack $ map (fromIntegral . ord) "/haskoin:0.0.1/"
+
+main = withSocketsDo $ do
+    h <- connectTo "127.0.0.1" (PortNumber 18333)
+    hSetBuffering h LineBuffering
+    time <- getPOSIXTime
+    nonce <- randomIO
+    let version = B.Version
+                    70001 1 (floor time) addr addr nonce userAgent 0 False
+        message = B.encodeMessage version
+    print version
+    BS.hPutStr h message
+    contents <- BL.hGetContents h
+    print $ B.readMessage contents
+    hClose h
 

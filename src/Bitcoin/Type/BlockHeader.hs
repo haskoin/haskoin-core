19ddccd4153d3767289c4028a43e5faf31ad3671
ciphermonk <ciphermonk@altera-vita.com>
2013-04-13 12:03:24 +0800

    Adding BlockHeader type

---
+++ src/Bitcoin/Type/BlockHeader.hs
@@ -0,0 +1,36 @@
+module Bitcoin.Type.BlockHeader ( BlockHeader(..) ) where
+
+import Data.Word
+import Data.Binary.Get
+import Data.Binary.Put
+import Control.Applicative
+
+import Bitcoin.Type.Hash
+
+import qualified Bitcoin.Type as Bitcoin
+
+data BlockHeader = BlockHeader {
+    blockVersion   :: Word32,
+    prevBlock      :: Hash,
+    merkleRoot     :: Hash,
+    blockTimestamp :: Word32,
+    blockBits      :: Word32,
+    nonce          :: Word32
+}
+
+instance Bitcoin.Type BlockHeader where
+    get = BlockHeader <$> Bitcoin.getWord32
+                      <*> Bitcoin.getHash
+                      <*> Bitcoin.getHash
+                      <*> Bitcoin.getWord32
+                      <*> Bitcoin.getWord32
+                      <*> Bitcoin.getWord32
+
+    put (BlockHeader v p m bt bb n) = do
+        Bitcoin.putWord32 v
+        Bitcoin.putHash   p
+        Bitcoin.putHash   m
+        Bitcoin.putWord32 bt
+        Bitcoin.putWord32 bb
+        Bitcoin.putWord32 n 
+

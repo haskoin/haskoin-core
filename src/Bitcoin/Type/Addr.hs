dd2ba479c74e053137fe664ac071a3d642cb3f43
ciphermonk <ciphermonk@altera-vita.com>
2013-04-13 13:58:03 +0800

    Adding the Addr type

---
+++ src/Bitcoin/Type/Addr.hs
@@ -0,0 +1,28 @@
+module Bitcoin.Type.Addr ( Addr(..) ) where
+
+import Control.Monad
+
+import Data.Word
+import Data.Binary.Get
+import Data.Binary.Put
+import Control.Applicative
+
+import qualified Bitcoin.Type as Bitcoin
+
+import Bitcoin.Type.NetworkAddress
+import Bitcoin.Type.VarInt
+
+data Addr = Addr {
+    addrList :: [(Word32, NetworkAddress)]
+}
+
+instance Bitcoin.Type Addr where
+    get = do
+        (VarInt count) <- Bitcoin.get :: Get VarInt
+        let action = liftM2 (,) Bitcoin.getWord32 Bitcoin.get
+        Addr <$> replicateM (fromIntegral count) action
+
+    put (Addr xs) = do
+        Bitcoin.put $ (VarInt . fromIntegral . length) xs
+        forM_ xs $ \(a,b) -> (Bitcoin.putWord32 a) >> (Bitcoin.put b)
+

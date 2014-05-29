b1db19742903718664f89b2f469beddce24152a4
Jean-Pierre Rupp <root@xeno-genesis.com>
2014-05-29 18:23:11 +0100

    Exposed all JSON-RPC modules

--- Network/Haskoin/Stratum.hs
+++ Network/Haskoin/Stratum.hs
@@ -28,10 +28,46 @@ module Network.Haskoin.Stratum
 , parseResult
 , parseNotif
 , newStratumReq
+
+  -- * JSON-RPC Messages
+  -- ** Types
+, Method
+, ErrorValue
+, RequestValue
+, ResponseValue
+, MsgValue
+, ResultValue
+, Id(..)
+, Result
+, Error(..)
+  -- ** Messages
+, Request(..)
+, Response(..)
+, Msg(..)
+  -- ** Errors
+, errParse
+, errReq
+, errMeth
+, errParams
+, errInternal
+, errStr
+  -- ** Helpers
+, leftStr
+, numericId
+
+  -- * JSON-RPC Conduit
+  -- ** Types
+, Session
+  -- ** Functions
+, initSession
+, newReq
+, newNotif
+, reqSource
+, resConduit
 ) where
 
--- import Network.Haskoin.Stratum.Message
--- import Network.Haskoin.Stratum.Conduit
+import Network.Haskoin.Stratum.Message
+import Network.Haskoin.Stratum.Conduit
 import Network.Haskoin.Stratum.Types
 import Network.Haskoin.Stratum.Exceptions
 import Network.Haskoin.Stratum.Client

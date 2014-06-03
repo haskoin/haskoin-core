ee8d199ce373c2a3840ae49ca0206e996634e1a2
Otto Allmendinger <otto.allmendinger@gmail.com>
2014-06-03 21:12:02 +0200

    protocol: instance Binary ScriptOp 'get' methods

--- Network/Haskoin/Protocol/Script.hs
+++ Network/Haskoin/Protocol/Script.hs
@@ -234,6 +234,7 @@ instance Binary ScriptOp where
                 len  <- getWord32le
                 payload <- getByteString (fromIntegral len)
                 return $ OP_PUSHDATA payload OPDATA4
+
             | op == 0x4f = return $ OP_1NEGATE
             | op == 0x51 = return $ OP_1
             | op == 0x52 = return $ OP_2
@@ -251,14 +252,113 @@ instance Binary ScriptOp where
             | op == 0x5e = return $ OP_14
             | op == 0x5f = return $ OP_15
             | op == 0x60 = return $ OP_16
+            -- Flow control
+            | op == 0x61 = return $ OP_NOP
+            -- | op == 0x62 = return $ OP_VER        -- reserved
+            | op == 0x63 = return $ OP_IF
+            | op == 0x64 = return $ OP_NOTIF
+            -- | op == 0x65 = return $ OP_VERIF      -- reserved
+            -- | op == 0x66 = return $ OP_VERNOTIF   -- reserved
+            | op == 0x67 = return $ OP_ELSE
+            | op == 0x68 = return $ OP_ENDIF
             | op == 0x69 = return $ OP_VERIFY
+            | op == 0x6a = return $ OP_RETURN
+
+            -- Stack
+            | op == 0x6b = return $ OP_TOALTSTACK
+            | op == 0x6c = return $ OP_FROMALTSTACK
+            | op == 0x6d = return $ OP_2DROP
+            | op == 0x6e = return $ OP_2DUP
+            | op == 0x6f = return $ OP_3DUP
+            | op == 0x70 = return $ OP_2OVER
+            | op == 0x71 = return $ OP_2ROT
+            | op == 0x72 = return $ OP_2SWAP
+            | op == 0x73 = return $ OP_IFDUP
+            | op == 0x74 = return $ OP_DEPTH
+            | op == 0x75 = return $ OP_DROP
             | op == 0x76 = return $ OP_DUP
+            | op == 0x77 = return $ OP_NIP
+            | op == 0x78 = return $ OP_OVER
+            | op == 0x79 = return $ OP_PICK
+            | op == 0x7a = return $ OP_ROLL
+            | op == 0x7b = return $ OP_ROT
+            | op == 0x7c = return $ OP_SWAP
+            | op == 0x7d = return $ OP_TUCK
+
+            -- Splice
+            | op == 0x7e = return $ OP_CAT
+            | op == 0x7f = return $ OP_SUBSTR
+            | op == 0x80 = return $ OP_LEFT
+            | op == 0x81 = return $ OP_RIGHT
+            | op == 0x82 = return $ OP_SIZE
+
+            -- Bitwise logic
+            | op == 0x83 = return $ OP_INVERT
+            | op == 0x84 = return $ OP_AND
+            | op == 0x85 = return $ OP_OR
+            | op == 0x86 = return $ OP_XOR
             | op == 0x87 = return $ OP_EQUAL
             | op == 0x88 = return $ OP_EQUALVERIFY
+            -- | op == 0x89 = return $ OP_RESERVED1
+            -- | op == 0x8a = return $ OP_RESERVED2
+
+            -- Arithmetic
+            | op == 0x8b = return $ OP_1ADD
+            | op == 0x8c = return $ OP_1SUB
+            | op == 0x8d = return $ OP_2MUL
+            | op == 0x8e = return $ OP_2DIV
+            | op == 0x8f = return $ OP_NEGATE
+            | op == 0x90 = return $ OP_ABS
+            | op == 0x91 = return $ OP_NOT
+            | op == 0x92 = return $ OP_0NOTEQUAL
+            | op == 0x93 = return $ OP_ADD
+            | op == 0x94 = return $ OP_SUB
+            | op == 0x95 = return $ OP_MUL
+            | op == 0x96 = return $ OP_DIV
+            | op == 0x97 = return $ OP_MOD
+            | op == 0x98 = return $ OP_LSHIFT
+            | op == 0x99 = return $ OP_RSHIFT
+            | op == 0x9a = return $ OP_BOOLAND
+            | op == 0x9b = return $ OP_BOOLOR
+            | op == 0x9c = return $ OP_NUMEQUAL
+            | op == 0x9d = return $ OP_NUMEQUALVERIFY
+            | op == 0x9e = return $ OP_NUMNOTEQUAL
+            | op == 0x9f = return $ OP_LESSTHAN
+            | op == 0xa0 = return $ OP_GREATERTHAN
+            | op == 0xa1 = return $ OP_LESSTHANOREQUAL
+            | op == 0xa2 = return $ OP_GREATERTHANOREQUAL
+            | op == 0xa3 = return $ OP_MIN
+            | op == 0xa4 = return $ OP_MAX
+            | op == 0xa5 = return $ OP_WITHIN
+
+            -- Crypto
+            | op == 0xa6 = return $ OP_RIPEMD160
+            | op == 0xa7 = return $ OP_SHA1
+            | op == 0xa8 = return $ OP_SHA256
             | op == 0xa9 = return $ OP_HASH160
+            | op == 0xaa = return $ OP_HASH256
+            | op == 0xab = return $ OP_CODESEPARATOR
             | op == 0xac = return $ OP_CHECKSIG
+            | op == 0xad = return $ OP_CHECKSIGVERIFY
             | op == 0xae = return $ OP_CHECKMULTISIG
+            | op == 0xaf = return $ OP_CHECKMULTISIGVERIFY
+
+            -- More NOPs
+            | op == 0xb0 = return $ OP_NOP1
+            | op == 0xb1 = return $ OP_NOP2
+            | op == 0xb2 = return $ OP_NOP3
+            | op == 0xb3 = return $ OP_NOP4
+            | op == 0xb4 = return $ OP_NOP5
+            | op == 0xb5 = return $ OP_NOP6
+            | op == 0xb6 = return $ OP_NOP7
+            | op == 0xb7 = return $ OP_NOP8
+            | op == 0xb8 = return $ OP_NOP9
+            | op == 0xb9 = return $ OP_NOP10
+
+            -- Constants
+            | op == 0xfd = OP_PUBKEYHASH <$> get
             | op == 0xfe = OP_PUBKEY <$> get
+
             | otherwise = return $ OP_INVALIDOPCODE op
 
     put op = case op of

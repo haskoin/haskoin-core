a0a12c34fd4984c876b290d6bd3cd109a846536c
Otto Allmendinger <otto.allmendinger@gmail.com>
2014-06-12 13:36:50 +0200

    script: add missing opcodes to Arbitrary ScriptOp

--- tests/Network/Haskoin/Script/Arbitrary.hs
+++ tests/Network/Haskoin/Script/Arbitrary.hs
@@ -32,20 +32,123 @@ instance Arbitrary Script where
 
 instance Arbitrary ScriptOp where
     arbitrary = oneof [ opPushData <$> nonEmptyBS
-                      , return OP_0
-                      , return OP_1NEGATE
-                      , return OP_1
-                      , return OP_2, return OP_3, return OP_4, return OP_5
-                      , return OP_6, return OP_7, return OP_8, return OP_9
-                      , return OP_10, return OP_11, return OP_12, return OP_13
-                      , return OP_14, return OP_15, return OP_16
-                      , return OP_VERIFY
-                      , return OP_DUP
-                      , return OP_EQUAL
-                      , return OP_EQUALVERIFY
-                      , return OP_HASH160
-                      , return OP_CHECKSIG
-                      , return OP_CHECKMULTISIG
+                      , return $ OP_1NEGATE
+                      , return $ OP_0
+                      , return $ OP_1
+                      , return $ OP_2
+                      , return $ OP_3
+                      , return $ OP_4
+                      , return $ OP_5
+                      , return $ OP_6
+                      , return $ OP_7
+                      , return $ OP_8
+                      , return $ OP_9
+                      , return $ OP_10
+                      , return $ OP_11
+                      , return $ OP_12
+                      , return $ OP_13
+                      , return $ OP_14
+                      , return $ OP_15
+                      , return $ OP_16
+
+                      -- Flow control
+                      , return $ OP_NOP
+                      , return $ OP_IF
+                      , return $ OP_NOTIF
+                      , return $ OP_ELSE
+                      , return $ OP_ENDIF
+                      , return $ OP_VERIFY
+                      , return $ OP_RETURN
+
+                      -- Stack
+                      , return $ OP_TOALTSTACK
+                      , return $ OP_FROMALTSTACK
+                      , return $ OP_2DROP
+                      , return $ OP_2DUP
+                      , return $ OP_3DUP
+                      , return $ OP_2OVER
+                      , return $ OP_2ROT
+                      , return $ OP_2SWAP
+                      , return $ OP_IFDUP
+                      , return $ OP_DEPTH
+                      , return $ OP_DROP
+                      , return $ OP_DUP
+                      , return $ OP_NIP
+                      , return $ OP_OVER
+                      , return $ OP_PICK
+                      , return $ OP_ROLL
+                      , return $ OP_ROT
+                      , return $ OP_SWAP
+                      , return $ OP_TUCK
+
+                      -- Splice
+                      , return $ OP_CAT
+                      , return $ OP_SUBSTR
+                      , return $ OP_LEFT
+                      , return $ OP_RIGHT
+                      , return $ OP_SIZE
+
+                      -- Bitwise logic
+                      , return $ OP_INVERT
+                      , return $ OP_AND
+                      , return $ OP_OR
+                      , return $ OP_XOR
+                      , return $ OP_EQUAL
+                      , return $ OP_EQUALVERIFY
+
+                      -- Arithmetic
+                      , return $ OP_1ADD
+                      , return $ OP_1SUB
+                      , return $ OP_2MUL
+                      , return $ OP_2DIV
+                      , return $ OP_NEGATE
+                      , return $ OP_ABS
+                      , return $ OP_NOT
+                      , return $ OP_0NOTEQUAL
+                      , return $ OP_ADD
+                      , return $ OP_SUB
+                      , return $ OP_MUL
+                      , return $ OP_DIV
+                      , return $ OP_MOD
+                      , return $ OP_LSHIFT
+                      , return $ OP_RSHIFT
+                      , return $ OP_BOOLAND
+                      , return $ OP_BOOLOR
+                      , return $ OP_NUMEQUAL
+                      , return $ OP_NUMEQUALVERIFY
+                      , return $ OP_NUMNOTEQUAL
+                      , return $ OP_LESSTHAN
+                      , return $ OP_GREATERTHAN
+                      , return $ OP_LESSTHANOREQUAL
+                      , return $ OP_GREATERTHANOREQUAL
+                      , return $ OP_MIN
+                      , return $ OP_MAX
+                      , return $ OP_WITHIN
+
+                      -- Crypto
+                      , return $ OP_RIPEMD160
+                      , return $ OP_SHA1
+                      , return $ OP_SHA256
+                      , return $ OP_HASH160
+                      , return $ OP_HASH256
+                      , return $ OP_CODESEPARATOR
+                      , return $ OP_CHECKSIG
+                      , return $ OP_CHECKSIGVERIFY
+                      , return $ OP_CHECKMULTISIG
+                      , return $ OP_CHECKMULTISIGVERIFY
+
+                      -- More NOPs
+                      , return $ OP_NOP1
+                      , return $ OP_NOP2
+                      , return $ OP_NOP3
+                      , return $ OP_NOP4
+                      , return $ OP_NOP5
+                      , return $ OP_NOP6
+                      , return $ OP_NOP7
+                      , return $ OP_NOP8
+                      , return $ OP_NOP9
+                      , return $ OP_NOP10
+
                       , return $ OP_INVALIDOPCODE 0xff
                       ]
 

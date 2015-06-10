43aae1ee77716d955238e305512509b371b1b629
plaprade <plaprade@gmail.com>
2015-06-10 20:46:46 +0200

    Serialize database requests

--- Network/Haskoin/Wallet/Server.hs
+++ Network/Haskoin/Wallet/Server.hs
@@ -7,9 +7,8 @@ import System.Posix.Daemon (runDetached, Redirection (ToFile), killAndWait)
 import System.ZMQ4
 
 import Control.Applicative ((<$>))
-import Control.Monad (when, void, unless, forM, forever, liftM)
+import Control.Monad (when, void, unless, forM, forever, liftM, join)
 import Control.Monad.Trans (MonadIO, lift, liftIO)
-import Control.Exception (SomeException(..),  tryJust, catch)
 import Control.Concurrent.STM.TBMChan (writeTBMChan)
 import Control.Concurrent.STM (atomically)
 import Control.Concurrent.Async.Lifted (withAsync, link)
@@ -18,6 +17,9 @@ import Control.Monad.Trans.Control (MonadBaseControl, control, liftBaseOp)
 import Control.Monad.Base (MonadBase)
 import Control.Monad.Catch (MonadThrow)
 import Control.Monad.Trans.Resource (MonadResource, runResourceT)
+import qualified Control.Exception as E 
+    (SomeException(..), ErrorCall(..), Handler(..), catches)
+import qualified Control.Concurrent.MSem as Sem (MSem, new)
 import Control.Monad.Logger 
     ( MonadLogger
     , runStdoutLoggingT
@@ -29,18 +31,14 @@ import Control.Monad.Logger
 
 import Data.Text (pack)
 import Data.Time.Clock.POSIX (getPOSIXTime)
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, fromMaybe)
 import Data.Aeson (Value, toJSON, decode, encode)
 import Data.Conduit (Sink, awaitForever, ($$))
 import Data.Conduit.TMChan (TBMChan, sourceTBMChan)
 import qualified Data.Map.Strict as M (unionsWith, null, toList, empty)
 
 import Database.Persist (get)
-import Database.Persist.Sql 
-    ( ConnectionPool
-    , runSqlPersistMPool
-    , runMigration
-    )
+import Database.Persist.Sql (ConnectionPool, runMigration)
 import qualified Database.LevelDB.Base as DB (Options(..), open, defaultOptions)
 
 import Network.Haskoin.Constants
@@ -61,148 +59,144 @@ filterLevel :: (LogLevel -> Bool) -> LoggingT m a -> LoggingT m a
 filterLevel p (LoggingT f) = LoggingT $ \logger ->
     f $ \loc src level msg -> when (p level) $ logger loc src level msg
 
+initDatabase :: Config -> IO (Sem.MSem Int, ConnectionPool)
+initDatabase cfg = do
+    -- Create a semaphore with 1 resource
+    sem <- Sem.new 1
+    -- Create a database pool
+    pool <- getDatabasePool $ configDatabase cfg
+    -- Initialize wallet database
+    runDBPool sem pool $ do
+        _ <- runMigration migrateWallet 
+        initWallet $ configBloomFP cfg
+    -- Return the semaphrone and the connection pool
+    return (sem, pool)
+
 runSPVServer :: Config -> IO ()
-runSPVServer cfg =
-    -- Start server process
-    maybeDetach cfg $ do
-        -- Get database pool
-        pool <- getDatabasePool $ configDatabase cfg
-        
-        -- Initialize wallet database
-        flip runSqlPersistMPool pool $ do 
-            _ <- runMigration migrateWallet 
-            initWallet $ configBloomFP cfg
+runSPVServer cfg = maybeDetach cfg $ do -- start the server process
+    -- Initialize the database
+    (sem, pool) <- initDatabase cfg
 
-        let logFilter level = level >= configLogLevel cfg
-            runLogging = runStdoutLoggingT . filterLevel logFilter
-            run        = runResourceT . runLogging
+    -- Setup logging monads
+    let logFilter level = level >= configLogLevel cfg
+        runLogging = runStdoutLoggingT . filterLevel logFilter
+        run = runResourceT . runLogging
 
-        if configMode cfg == SPVOffline
-            then run $ runWalletApp $ HandlerSession cfg pool Nothing
-            else do
-                -- Create leveldb handle
-                db <- DB.open "headertree"
-                    DB.defaultOptions{ DB.createIfMissing = True
-                                     , DB.cacheSize       = 2048
-                                     }
+    -- Check the operation mode of the server.
+    case configMode cfg of
+        -- In this mode, we do not launch an SPV node. We only accept
+        -- client requests through the ZMQ API.
+        SPVOffline -> run $ runWalletApp $ HandlerSession cfg pool Nothing sem
+        -- In this mode, we launch the client ZMQ API and we sync the
+        -- wallet database with an SPV node.
+        SPVOnline -> do
+            -- Create leveldb handle
+            db <- DB.open "headertree"
+                DB.defaultOptions{ DB.createIfMissing = True
+                                 , DB.cacheSize       = 2048
+                                 }
 
-                -- Get our best block or compute a fast catchup time otherwise
-                (best, height) <- runSqlPersistMPool getBestBlock pool
-                dwnE <- if height > 0
-                    -- If we have a best block, use it to download merkles
-                    then return $ Right best
-                    -- Otherwise, give the node a fast catchup time
-                    else do
-                        fstKeyTimeM <- runSqlPersistMPool firstAddrTime pool
-                        Left . adjustFCTime <$> case fstKeyTimeM of
-                            Just ts  -> return ts
-                            -- If we have no keys, use the current time
-                            Nothing -> round <$> getPOSIXTime
+            -- Get our best block or compute a fast catchup time otherwise
+            (best, height) <- runDBPool sem pool getBestBlock
+            dwnE <- if height > 0
+                -- If we have a best block, use it to download merkles
+                then return $ Right best
+                -- Otherwise, give the node a fast catchup time
+                else do
+                    fstKeyTimeM <- runDBPool sem pool firstAddrTime
+                    Left . adjustFCTime <$> case fstKeyTimeM of
+                        Just ts  -> return ts
+                        -- If we have no keys, use the current time
+                        Nothing -> round <$> getPOSIXTime
 
-                    -- Bitcoin nodes to connect to
-                let nodes = configBTCNodes cfg 
-                    -- Run the SPV monad stack
-                    runNode = run . (`evalStateT` db)
+                -- Bitcoin nodes to connect to
+            let nodes = configBTCNodes cfg 
+                -- Run the SPV monad stack
+                runNode = run . (`evalStateT` db)
 
-                -- Compute our bloom filter
-                bloom <- liftM fst3 $ runSqlPersistMPool getBloomFilter pool
+            -- Compute our bloom filter
+            bloom <- liftM fst3 $ runDBPool sem pool getBloomFilter
 
-                -- Launch SPV node
-                runNode $ withNode $ \eChan rChan -> do
-                    -- Connect to remote nodes
-                    liftIO . atomically $ writeTBMChan rChan $
-                        NodeConnectPeers $ map (uncurry RemoteHost) nodes
-                    -- Send our bloom filter
-                    liftIO . atomically $ writeTBMChan rChan $
-                        NodeBloomFilter bloom
-                    -- Start the merkle block download process
-                    liftIO . atomically $ writeTBMChan rChan $
-                        NodeStartMerkleDownload dwnE
+            -- Launch SPV node
+            runNode $ withNode $ \eChan rChan -> do
+                -- Connect to remote nodes
+                liftIO . atomically $ writeTBMChan rChan $
+                    NodeConnectPeers $ map (uncurry RemoteHost) nodes
+                -- Send our bloom filter
+                liftIO . atomically $ writeTBMChan rChan $
+                    NodeBloomFilter bloom
+                -- Start the merkle block download process
+                liftIO . atomically $ writeTBMChan rChan $
+                    NodeStartMerkleDownload dwnE
 
-                    -- Listen to SPV events and update the wallet database
-                    let runEvents = sourceTBMChan eChan $$ 
-                                    processEvents rChan pool
+                -- Listen to SPV events and update the wallet database
+                let runEvents = sourceTBMChan eChan $$ 
+                                processEvents rChan sem pool
 
-                    withAsync runEvents $ \a -> do
-                        link a
-                        -- Run the zeromq server listening to user requests
-                        runWalletApp $ HandlerSession cfg pool $ Just rChan
+                withAsync runEvents $ \a -> do
+                    link a
+                    -- Run the zeromq server listening to user requests
+                    runWalletApp $ HandlerSession cfg pool (Just rChan) sem
 
 processEvents :: (MonadLogger m, MonadIO m, Functor m)
-              => TBMChan NodeRequest -> ConnectionPool
+              => TBMChan NodeRequest -> Sem.MSem Int -> ConnectionPool
               -> Sink WalletMessage m ()
-processEvents rChan pool = awaitForever $ \req -> lift $ case req of
+processEvents rChan sem pool = awaitForever $ \req -> lift $ case req of
     WalletTx tx -> void (processTxs [tx])
     WalletMerkles action dmbs -> do
         -- Save the old best block before importing
-        oldBestE <- liftIO $ tryJust f $ runSqlPersistMPool getBestBlock pool
-        oldBest <- case oldBestE of
-            Right (ob, _) -> return ob
-            Left err -> do
-                $(logError) $ pack $ unwords
-                    [ "processEvents: An error occured:", err ]
-                return $ headerHash genesisHeader
+        oldBestM <- tryDBPool sem pool getBestBlock
+        let oldBest = maybe (headerHash genesisHeader) fst oldBestM
 
         -- Import all transactions into the wallet
         let txs = concatMap merkleTxs dmbs
-        xs <- processTxs txs
+        newAddrsMap <- processTxs txs
 
         -- Import the merkle blocks into the wallet
-        resE <- liftIO $ tryJust f $ flip runSqlPersistMPool pool $ 
-            importMerkles action $ map expectedTxs dmbs
-        when (isLeft resE) $ $(logError) $ pack $ unwords
-            [ "processEvents: An error occured:", fromLeft resE ]
+        _ <- tryDBPool sem pool $ importMerkles action $ map expectedTxs dmbs
 
         -- If we received at least 1 transaction, the node will block the
         -- merkle block download and wait for us to continue the download.
         unless (null txs) $ do
-            -- For every account, check if we busted the gap.
-            scanE <- liftIO $ tryJust f $ flip runSqlPersistMPool pool $
-                forM (M.toList xs) $ \(ai, cnt) -> do
-                    accM <- get ai
-                    return $ maybe False ((cnt >) . keyRingAccountGap) accM
-
-            case scanE of
-                Right ys -> do
-                    when (or ys) $ $(logDebug) $ pack $ unwords
-                        [ "Generated addresses beyond the account gap."
-                        , "Rescanning this batch."
-                        ]
-                           -- If we use addresses in the hidden gap, we must
-                           -- rescan this batch.
-                    let bh | or ys = oldBest
-                           -- Otherwise, simply continue the merkle download
-                           -- from the new best block
-                           | otherwise = 
-                               nodeBlockHash $ last $ actionNewNodes action
-                    -- Send a message to the node to continue the download from
-                    -- the requested block hash
-                    liftIO . atomically $ writeTBMChan rChan $ 
-                        NodeStartMerkleDownload $ Right bh
-                Left err -> $(logError) $ pack $ unwords
-                    [ "processEvents: An error occured:", err]
-
-    _ -> return () -- Ignore full blocks
+            -- Do we have to rescan the current batch ?
+            rescan <- shouldRescan $ M.toList newAddrsMap
+                    -- If we use addresses in the hidden gap, we must
+                    -- rescan this batch.
+            let bh | rescan = oldBest
+                   -- Otherwise, simply continue the merkle download
+                   -- from the new best block
+                   | otherwise = nodeBlockHash $ last $ actionNewNodes action
+            when rescan $ $(logDebug) $ pack $ unwords
+                [ "Generated addresses beyond the account gap."
+                , "Rescanning this batch."
+                ]
+            -- Send a message to the node to continue the download from
+            -- the requested block hash
+            liftIO . atomically $ 
+                writeTBMChan rChan $ NodeStartMerkleDownload $ Right bh
+    -- Ignore full blocks
+    _ -> return ()
   where
+    shouldRescan accs = case accs of
+        ((ai, cnt):rest) -> do
+            accM <- liftM join $ tryDBPool sem pool $ get ai
+            -- For every account, check if we busted the gap.
+            if maybe False ((cnt >) . keyRingAccountGap) accM
+                then return True
+                else shouldRescan rest
+        _ -> return False
     processTxs txs = do
-        resE <- liftIO $ tryJust f $ flip runSqlPersistMPool pool $ do
-            -- Import all transactions into the wallet as network transactions
-            xs <- liftM (map snd . catMaybes) $ forM txs importNetTx
-            (bloom, _, _) <- getBloomFilter
-            -- Return the count of new addresses created for each account
-            return (bloom, M.unionsWith (+) xs)
-        case resE of
-            Right (bloom, xs) -> do
-                -- When new addresses were created, we need to send the new
-                -- bloom filter to our peers.
-                unless (M.null xs) $ liftIO . atomically $ 
+        -- Import all the transactions into the wallet as network transactions
+        resM <- liftM catMaybes $ forM txs $ tryDBPool sem pool . importNetTx
+        let newAddrsMap = M.unionsWith (+) $ map snd $ catMaybes resM
+        -- Send the bloom filter to peers when new addresses were created
+        unless (M.null newAddrsMap) $ do
+            bloomM <- tryDBPool sem pool $ getBloomFilter
+            case bloomM of
+                Just (bloom, _, _) -> liftIO . atomically $ 
                     writeTBMChan rChan $ NodeBloomFilter bloom
-                return xs
-            Left err -> do
-                $(logError) $ pack $ unwords 
-                    [ "processEvents: An error occured:", err ]
-                return M.empty
-    f (SomeException e) = Just $ show e
+                _ -> return ()
+        return newAddrsMap
 
 maybeDetach :: Config -> IO () -> IO ()
 maybeDetach cfg action =
@@ -217,6 +211,8 @@ stopSPVServer cfg =
     killAndWait $ configPidFile cfg
 
 -- Run the main ZeroMQ loop
+-- TODO: Support concurrent requests using DEALER socket when we can do
+-- concurrent MySQL requests.
 runWalletApp :: ( MonadIO m
                 , MonadLogger m
                 , MonadBaseControl IO m
@@ -235,9 +231,14 @@ runWalletApp session = liftBaseOp withContext $ \ctx ->
                 Nothing -> return $ ResponseError "Could not decode request"
             liftIO $ send sock [] $ toStrictBS $ encode res
   where
-    -- TODO: Catch ErrorCall and SomeException
-    catchErrors m = control $ \runInIO -> catch (runInIO m) $ 
-        \(WalletException err) -> runInIO $ return $ ResponseError $ pack err
+    catchErrors m = control $ \runInIO -> E.catches (runInIO m) 
+        [ E.Handler (\(WalletException err) -> runInIO $ 
+            return $ ResponseError $ pack err)
+        , E.Handler (\(E.ErrorCall err) -> runInIO $ 
+            return $ ResponseError $ pack err)
+        , E.Handler (\(E.SomeException exc) -> runInIO $ 
+            return $ ResponseError $ pack $ show exc)
+        ]
 
 dispatchRequest :: ( MonadLogger m
                    , MonadBaseControl IO m

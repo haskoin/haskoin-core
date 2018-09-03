6effef58aaf92c1dbf2239af0c1aab66b9f0eaba
Jean-Pierre Rupp <xenog@protonmail.com>
2018-09-03 19:53:03 +0100

    CashAddr tests typecheck

--- test/Network/Haskoin/Address/CashAddrSpec.hs
+++ test/Network/Haskoin/Address/CashAddrSpec.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.Haskoin.Address.CashAddrSpec (spec) where
 
+import           Control.Monad
+import           Data.ByteString                  (ByteString, length)
+import           Data.ByteString.Char8            (pack)
+import           Data.Maybe
+import           Network.Haskoin.Address
 import           Network.Haskoin.Address.CashAddr
 import           Network.Haskoin.Constants
 import           Network.Haskoin.Crypto
-import           Network.Haskoin.Address
 import           Network.Haskoin.Util
 import           Test.Hspec
-import           Test.HUnit                     (Assertion, assertBool)
-import           Data.ByteString                ( ByteString, length )
-import           Data.ByteString.Char8          ( pack )
+import           Test.HUnit                       (Assertion, assertBool)
 
 spec = do
   describe "cashaddr checksum test vectors" $ do
@@ -70,38 +72,8 @@ spec = do
       addr `shouldBe` Just "31nwvkZwyPdgzjBJZXfDmSWsC4ZLKpYyUw"
 
   describe "cashAddress larger test vectors" $ do
-    it "cashAddr test vector 1" (testCashAddr $ head vectors)
-    it "cashAddr test vector 2" (testCashAddr $ vectors !! 1)
-    it "cashAddr test vector 3" (testCashAddr $ vectors !! 2)
-    it "cashAddr test vector 4" (testCashAddr $ vectors !! 3)
-    it "cashAddr test vector 5" (testCashAddr $ vectors !! 4)
-    it "cashAddr test vector 6" (testCashAddr $ vectors !! 5)
-    it "cashAddr test vector 7" (testCashAddr $ vectors !! 6)
-    it "cashAddr test vector 8" (testCashAddr $ vectors !! 7)
-    it "cashAddr test vector 9" (testCashAddr $ vectors !! 8)
-    it "cashAddr test vector 10" (testCashAddr $ vectors !! 9)
-    it "cashAddr test vector 11" (testCashAddr $ vectors !! 10)
-    it "cashAddr test vector 12" (testCashAddr $ vectors !! 11)
-    it "cashAddr test vector 13" (testCashAddr $ vectors !! 12)
-    it "cashAddr test vector 14" (testCashAddr $ vectors !! 13)
-    it "cashAddr test vector 15" (testCashAddr $ vectors !! 14)
-    it "cashAddr test vector 16" (testCashAddr $ vectors !! 15)
-    it "cashAddr test vector 17" (testCashAddr $ vectors !! 16)
-    it "cashAddr test vector 18" (testCashAddr $ vectors !! 17)
-    it "cashAddr test vector 19" (testCashAddr $ vectors !! 18)
-    it "cashAddr test vector 20" (testCashAddr $ vectors !! 19)
-    it "cashAddr test vector 21" (testCashAddr $ vectors !! 20)
-    it "cashAddr test vector 22" (testCashAddr $ vectors !! 21)
-    it "cashAddr test vector 23" (testCashAddr $ vectors !! 22)
-    it "cashAddr test vector 24" (testCashAddr $ vectors !! 23)
-    it "cashAddr test vector 25" (testCashAddr $ vectors !! 24)
-    it "cashAddr test vector 26" (testCashAddr $ vectors !! 25)
-    it "cashAddr test vector 27" (testCashAddr $ vectors !! 26)
-    it "cashAddr test vector 28" (testCashAddr $ vectors !! 27)
-    it "cashAddr test vector 29" (testCashAddr $ vectors !! 28)
-    it "cashAddr test vector 30" (testCashAddr $ vectors !! 29)
-    it "cashAddr test vector 31" (testCashAddr $ vectors !! 30)
-    it "cashAddr test vector 32" (testCashAddr $ vectors !! 31)
+      forM_ (zip [0..] vectors) $ \(i, vec) ->
+          it ("cashaddr test vector " <> show i) $ testCashAddr vec
 
 {- Various utilities -}
 
@@ -117,47 +89,148 @@ cashAddrtoBase58 c32 = fromCashAddr c32 >>= toBase58
     fromCashAddr = stringToAddr bch
     toBase58 a = addrToString a { getAddrNet = btc }
 
+testCashAddr :: (Int, CashVersion, Cash32, ByteString) -> Assertion
 testCashAddr (len, typ, addr, hex) = do
+    let mbs = decodeHex hex
+    mbs `shouldSatisfy` isJust
+    let mdec = cash32decodeType addr
+    mdec `shouldSatisfy` isJust
+    let Just (pfx, ver, pay) = mdec
     assertBool "Length" $ len == Data.ByteString.length pay
     assertBool "Version" $ ver == typ
     assertBool "Payload" $ pay == bs
   where
-    Just bs = decodeHex $ pack hex
-    Just (pfx, ver, pay) = cash32decodeType $ pack addr
+    Just bs = decodeHex hex
+    Just (pfx, ver, pay) = cash32decodeType addr
 
-vectors :: [(Int, Int, String, String)]
+vectors :: [(Int, CashVersion, ByteString, ByteString)]
 vectors =
-    [ (20, 0, "bitcoincash:qr6m7j9njldwwzlg9v7v53unlr4jkmx6eylep8ekg2","F5BF48B397DAE70BE82B3CCA4793F8EB2B6CDAC9")
-    , (20, 1, "bchtest:pr6m7j9njldwwzlg9v7v53unlr4jkmx6eyvwc0uz5t","F5BF48B397DAE70BE82B3CCA4793F8EB2B6CDAC9")
-    , (20, 0, "pref:pr6m7j9njldwwzlg9v7v53unlr4jkmx6ey65nvtks5","F5BF48B397DAE70BE82B3CCA4793F8EB2B6CDAC9")
-    , (20, 15, "prefix:0r6m7j9njldwwzlg9v7v53unlr4jkmx6ey3qnjwsrf","F5BF48B397DAE70BE82B3CCA4793F8EB2B6CDAC9")
-    , (24, 0, "bitcoincash:q9adhakpwzztepkpwp5z0dq62m6u5v5xtyj7j3h2ws4mr9g0","7ADBF6C17084BC86C1706827B41A56F5CA32865925E946EA")
-    , (24, 1, "bchtest:p9adhakpwzztepkpwp5z0dq62m6u5v5xtyj7j3h2u94tsynr","7ADBF6C17084BC86C1706827B41A56F5CA32865925E946EA")
-    , (24, 0, "pref:p9adhakpwzztepkpwp5z0dq62m6u5v5xtyj7j3h2khlwwk5v","7ADBF6C17084BC86C1706827B41A56F5CA32865925E946EA")
-    , (24, 15, "prefix:09adhakpwzztepkpwp5z0dq62m6u5v5xtyj7j3h2p29kc2lp","7ADBF6C17084BC86C1706827B41A56F5CA32865925E946EA")
-    , (28, 0, "bitcoincash:qgagf7w02x4wnz3mkwnchut2vxphjzccwxgjvvjmlsxqwkcw59jxxuz","3A84F9CF51AAE98A3BB3A78BF16A6183790B18719126325BFC0C075B")
-    , (28, 1, "bchtest:pgagf7w02x4wnz3mkwnchut2vxphjzccwxgjvvjmlsxqwkcvs7md7wt","3A84F9CF51AAE98A3BB3A78BF16A6183790B18719126325BFC0C075B")
-    , (28, 0, "pref:pgagf7w02x4wnz3mkwnchut2vxphjzccwxgjvvjmlsxqwkcrsr6gzkn","3A84F9CF51AAE98A3BB3A78BF16A6183790B18719126325BFC0C075B")
-    , (28, 15, "prefix:0gagf7w02x4wnz3mkwnchut2vxphjzccwxgjvvjmlsxqwkc5djw8s9g","3A84F9CF51AAE98A3BB3A78BF16A6183790B18719126325BFC0C075B")
-    , (32, 0, "bitcoincash:qvch8mmxy0rtfrlarg7ucrxxfzds5pamg73h7370aa87d80gyhqxq5nlegake","3173EF6623C6B48FFD1A3DCC0CC6489B0A07BB47A37F47CFEF4FE69DE825C060")
-    , (32, 1, "bchtest:pvch8mmxy0rtfrlarg7ucrxxfzds5pamg73h7370aa87d80gyhqxq7fqng6m6","3173EF6623C6B48FFD1A3DCC0CC6489B0A07BB47A37F47CFEF4FE69DE825C060")
-    , (32, 0, "pref:pvch8mmxy0rtfrlarg7ucrxxfzds5pamg73h7370aa87d80gyhqxq4k9m7qf9","3173EF6623C6B48FFD1A3DCC0CC6489B0A07BB47A37F47CFEF4FE69DE825C060")
-    , (32, 15, "prefix:0vch8mmxy0rtfrlarg7ucrxxfzds5pamg73h7370aa87d80gyhqxqsh6jgp6w","3173EF6623C6B48FFD1A3DCC0CC6489B0A07BB47A37F47CFEF4FE69DE825C060")
-    , (40, 0, "bitcoincash:qnq8zwpj8cq05n7pytfmskuk9r4gzzel8qtsvwz79zdskftrzxtar994cgutavfklv39gr3uvz","C07138323E00FA4FC122D3B85B9628EA810B3F381706385E289B0B25631197D194B5C238BEB136FB")
-    , (40, 1, "bchtest:pnq8zwpj8cq05n7pytfmskuk9r4gzzel8qtsvwz79zdskftrzxtar994cgutavfklvmgm6ynej","C07138323E00FA4FC122D3B85B9628EA810B3F381706385E289B0B25631197D194B5C238BEB136FB")
-    , (40, 0, "pref:pnq8zwpj8cq05n7pytfmskuk9r4gzzel8qtsvwz79zdskftrzxtar994cgutavfklv0vx5z0w3","C07138323E00FA4FC122D3B85B9628EA810B3F381706385E289B0B25631197D194B5C238BEB136FB")
-    , (40, 15, "prefix:0nq8zwpj8cq05n7pytfmskuk9r4gzzel8qtsvwz79zdskftrzxtar994cgutavfklvwsvctzqy","C07138323E00FA4FC122D3B85B9628EA810B3F381706385E289B0B25631197D194B5C238BEB136FB")
-    , (48, 0, "bitcoincash:qh3krj5607v3qlqh5c3wq3lrw3wnuxw0sp8dv0zugrrt5a3kj6ucysfz8kxwv2k53krr7n933jfsunqex2w82sl","E361CA9A7F99107C17A622E047E3745D3E19CF804ED63C5C40C6BA763696B98241223D8CE62AD48D863F4CB18C930E4C")
-    , (48, 1, "bchtest:ph3krj5607v3qlqh5c3wq3lrw3wnuxw0sp8dv0zugrrt5a3kj6ucysfz8kxwv2k53krr7n933jfsunqnzf7mt6x","E361CA9A7F99107C17A622E047E3745D3E19CF804ED63C5C40C6BA763696B98241223D8CE62AD48D863F4CB18C930E4C")
-    , (48, 0, "pref:ph3krj5607v3qlqh5c3wq3lrw3wnuxw0sp8dv0zugrrt5a3kj6ucysfz8kxwv2k53krr7n933jfsunqjntdfcwg","E361CA9A7F99107C17A622E047E3745D3E19CF804ED63C5C40C6BA763696B98241223D8CE62AD48D863F4CB18C930E4C")
-    , (48, 15, "prefix:0h3krj5607v3qlqh5c3wq3lrw3wnuxw0sp8dv0zugrrt5a3kj6ucysfz8kxwv2k53krr7n933jfsunqakcssnmn","E361CA9A7F99107C17A622E047E3745D3E19CF804ED63C5C40C6BA763696B98241223D8CE62AD48D863F4CB18C930E4C")
-    , (56, 0, "bitcoincash:qmvl5lzvdm6km38lgga64ek5jhdl7e3aqd9895wu04fvhlnare5937w4ywkq57juxsrhvw8ym5d8qx7sz7zz0zvcypqscw8jd03f","D9FA7C4C6EF56DC4FF423BAAE6D495DBFF663D034A72D1DC7D52CBFE7D1E6858F9D523AC0A7A5C34077638E4DD1A701BD017842789982041")
-    , (56, 1, "bchtest:pmvl5lzvdm6km38lgga64ek5jhdl7e3aqd9895wu04fvhlnare5937w4ywkq57juxsrhvw8ym5d8qx7sz7zz0zvcypqs6kgdsg2g","D9FA7C4C6EF56DC4FF423BAAE6D495DBFF663D034A72D1DC7D52CBFE7D1E6858F9D523AC0A7A5C34077638E4DD1A701BD017842789982041")
-    , (56, 0, "pref:pmvl5lzvdm6km38lgga64ek5jhdl7e3aqd9895wu04fvhlnare5937w4ywkq57juxsrhvw8ym5d8qx7sz7zz0zvcypqsammyqffl","D9FA7C4C6EF56DC4FF423BAAE6D495DBFF663D034A72D1DC7D52CBFE7D1E6858F9D523AC0A7A5C34077638E4DD1A701BD017842789982041")
-    , (56, 15, "prefix:0mvl5lzvdm6km38lgga64ek5jhdl7e3aqd9895wu04fvhlnare5937w4ywkq57juxsrhvw8ym5d8qx7sz7zz0zvcypqsgjrqpnw8","D9FA7C4C6EF56DC4FF423BAAE6D495DBFF663D034A72D1DC7D52CBFE7D1E6858F9D523AC0A7A5C34077638E4DD1A701BD017842789982041")
-    , (64, 0, "bitcoincash:qlg0x333p4238k0qrc5ej7rzfw5g8e4a4r6vvzyrcy8j3s5k0en7calvclhw46hudk5flttj6ydvjc0pv3nchp52amk97tqa5zygg96mtky5sv5w","D0F346310D5513D9E01E299978624BA883E6BDA8F4C60883C10F28C2967E67EC77ECC7EEEAEAFC6DA89FAD72D11AC961E164678B868AEEEC5F2C1DA08884175B")
-    , (64, 1, "bchtest:plg0x333p4238k0qrc5ej7rzfw5g8e4a4r6vvzyrcy8j3s5k0en7calvclhw46hudk5flttj6ydvjc0pv3nchp52amk97tqa5zygg96mc773cwez","D0F346310D5513D9E01E299978624BA883E6BDA8F4C60883C10F28C2967E67EC77ECC7EEEAEAFC6DA89FAD72D11AC961E164678B868AEEEC5F2C1DA08884175B")
-    , (64, 0, "pref:plg0x333p4238k0qrc5ej7rzfw5g8e4a4r6vvzyrcy8j3s5k0en7calvclhw46hudk5flttj6ydvjc0pv3nchp52amk97tqa5zygg96mg7pj3lh8","D0F346310D5513D9E01E299978624BA883E6BDA8F4C60883C10F28C2967E67EC77ECC7EEEAEAFC6DA89FAD72D11AC961E164678B868AEEEC5F2C1DA08884175B")
-    , (64, 15, "prefix:0lg0x333p4238k0qrc5ej7rzfw5g8e4a4r6vvzyrcy8j3s5k0en7calvclhw46hudk5flttj6ydvjc0pv3nchp52amk97tqa5zygg96ms92w6845", "D0F346310D5513D9E01E299978624BA883E6BDA8F4C60883C10F28C2967E67EC77ECC7EEEAEAFC6DA89FAD72D11AC961E164678B868AEEEC5F2C1DA08884175B")
+    [ ( 20
+      , 0
+      , "bitcoincash:qr6m7j9njldwwzlg9v7v53unlr4jkmx6eylep8ekg2"
+      , "F5BF48B397DAE70BE82B3CCA4793F8EB2B6CDAC9")
+    , ( 20
+      , 1
+      , "bchtest:pr6m7j9njldwwzlg9v7v53unlr4jkmx6eyvwc0uz5t"
+      , "F5BF48B397DAE70BE82B3CCA4793F8EB2B6CDAC9")
+    , ( 20
+      , 0
+      , "pref:pr6m7j9njldwwzlg9v7v53unlr4jkmx6ey65nvtks5"
+      , "F5BF48B397DAE70BE82B3CCA4793F8EB2B6CDAC9")
+    , ( 20
+      , 15
+      , "prefix:0r6m7j9njldwwzlg9v7v53unlr4jkmx6ey3qnjwsrf"
+      , "F5BF48B397DAE70BE82B3CCA4793F8EB2B6CDAC9")
+    , ( 24
+      , 0
+      , "bitcoincash:q9adhakpwzztepkpwp5z0dq62m6u5v5xtyj7j3h2ws4mr9g0"
+      , "7ADBF6C17084BC86C1706827B41A56F5CA32865925E946EA")
+    , ( 24
+      , 1
+      , "bchtest:p9adhakpwzztepkpwp5z0dq62m6u5v5xtyj7j3h2u94tsynr"
+      , "7ADBF6C17084BC86C1706827B41A56F5CA32865925E946EA")
+    , ( 24
+      , 0
+      , "pref:p9adhakpwzztepkpwp5z0dq62m6u5v5xtyj7j3h2khlwwk5v"
+      , "7ADBF6C17084BC86C1706827B41A56F5CA32865925E946EA")
+    , ( 24
+      , 15
+      , "prefix:09adhakpwzztepkpwp5z0dq62m6u5v5xtyj7j3h2p29kc2lp"
+      , "7ADBF6C17084BC86C1706827B41A56F5CA32865925E946EA")
+    , ( 28
+      , 0
+      , "bitcoincash:qgagf7w02x4wnz3mkwnchut2vxphjzccwxgjvvjmlsxqwkcw59jxxuz"
+      , "3A84F9CF51AAE98A3BB3A78BF16A6183790B18719126325BFC0C075B")
+    , ( 28
+      , 1
+      , "bchtest:pgagf7w02x4wnz3mkwnchut2vxphjzccwxgjvvjmlsxqwkcvs7md7wt"
+      , "3A84F9CF51AAE98A3BB3A78BF16A6183790B18719126325BFC0C075B")
+    , ( 28
+      , 0
+      , "pref:pgagf7w02x4wnz3mkwnchut2vxphjzccwxgjvvjmlsxqwkcrsr6gzkn"
+      , "3A84F9CF51AAE98A3BB3A78BF16A6183790B18719126325BFC0C075B")
+    , ( 28
+      , 15
+      , "prefix:0gagf7w02x4wnz3mkwnchut2vxphjzccwxgjvvjmlsxqwkc5djw8s9g"
+      , "3A84F9CF51AAE98A3BB3A78BF16A6183790B18719126325BFC0C075B")
+    , ( 32
+      , 0
+      , "bitcoincash:qvch8mmxy0rtfrlarg7ucrxxfzds5pamg73h7370aa87d80gyhqxq5nlegake"
+      , "3173EF6623C6B48FFD1A3DCC0CC6489B0A07BB47A37F47CFEF4FE69DE825C060")
+    , ( 32
+      , 1
+      , "bchtest:pvch8mmxy0rtfrlarg7ucrxxfzds5pamg73h7370aa87d80gyhqxq7fqng6m6"
+      , "3173EF6623C6B48FFD1A3DCC0CC6489B0A07BB47A37F47CFEF4FE69DE825C060")
+    , ( 32
+      , 0
+      , "pref:pvch8mmxy0rtfrlarg7ucrxxfzds5pamg73h7370aa87d80gyhqxq4k9m7qf9"
+      , "3173EF6623C6B48FFD1A3DCC0CC6489B0A07BB47A37F47CFEF4FE69DE825C060")
+    , ( 32
+      , 15
+      , "prefix:0vch8mmxy0rtfrlarg7ucrxxfzds5pamg73h7370aa87d80gyhqxqsh6jgp6w"
+      , "3173EF6623C6B48FFD1A3DCC0CC6489B0A07BB47A37F47CFEF4FE69DE825C060")
+    , ( 40
+      , 0
+      , "bitcoincash:qnq8zwpj8cq05n7pytfmskuk9r4gzzel8qtsvwz79zdskftrzxtar994cgutavfklv39gr3uvz"
+      , "C07138323E00FA4FC122D3B85B9628EA810B3F381706385E289B0B25631197D194B5C238BEB136FB")
+    , ( 40
+      , 1
+      , "bchtest:pnq8zwpj8cq05n7pytfmskuk9r4gzzel8qtsvwz79zdskftrzxtar994cgutavfklvmgm6ynej"
+      , "C07138323E00FA4FC122D3B85B9628EA810B3F381706385E289B0B25631197D194B5C238BEB136FB")
+    , ( 40
+      , 0
+      , "pref:pnq8zwpj8cq05n7pytfmskuk9r4gzzel8qtsvwz79zdskftrzxtar994cgutavfklv0vx5z0w3"
+      , "C07138323E00FA4FC122D3B85B9628EA810B3F381706385E289B0B25631197D194B5C238BEB136FB")
+    , ( 40
+      , 15
+      , "prefix:0nq8zwpj8cq05n7pytfmskuk9r4gzzel8qtsvwz79zdskftrzxtar994cgutavfklvwsvctzqy"
+      , "C07138323E00FA4FC122D3B85B9628EA810B3F381706385E289B0B25631197D194B5C238BEB136FB")
+    , ( 48
+      , 0
+      , "bitcoincash:qh3krj5607v3qlqh5c3wq3lrw3wnuxw0sp8dv0zugrrt5a3kj6ucysfz8kxwv2k53krr7n933jfsunqex2w82sl"
+      , "E361CA9A7F99107C17A622E047E3745D3E19CF804ED63C5C40C6BA763696B98241223D8CE62AD48D863F4CB18C930E4C")
+    , ( 48
+      , 1
+      , "bchtest:ph3krj5607v3qlqh5c3wq3lrw3wnuxw0sp8dv0zugrrt5a3kj6ucysfz8kxwv2k53krr7n933jfsunqnzf7mt6x"
+      , "E361CA9A7F99107C17A622E047E3745D3E19CF804ED63C5C40C6BA763696B98241223D8CE62AD48D863F4CB18C930E4C")
+    , ( 48
+      , 0
+      , "pref:ph3krj5607v3qlqh5c3wq3lrw3wnuxw0sp8dv0zugrrt5a3kj6ucysfz8kxwv2k53krr7n933jfsunqjntdfcwg"
+      , "E361CA9A7F99107C17A622E047E3745D3E19CF804ED63C5C40C6BA763696B98241223D8CE62AD48D863F4CB18C930E4C")
+    , ( 48
+      , 15
+      , "prefix:0h3krj5607v3qlqh5c3wq3lrw3wnuxw0sp8dv0zugrrt5a3kj6ucysfz8kxwv2k53krr7n933jfsunqakcssnmn"
+      , "E361CA9A7F99107C17A622E047E3745D3E19CF804ED63C5C40C6BA763696B98241223D8CE62AD48D863F4CB18C930E4C")
+    , ( 56
+      , 0
+      , "bitcoincash:qmvl5lzvdm6km38lgga64ek5jhdl7e3aqd9895wu04fvhlnare5937w4ywkq57juxsrhvw8ym5d8qx7sz7zz0zvcypqscw8jd03f"
+      , "D9FA7C4C6EF56DC4FF423BAAE6D495DBFF663D034A72D1DC7D52CBFE7D1E6858F9D523AC0A7A5C34077638E4DD1A701BD017842789982041")
+    , ( 56
+      , 1
+      , "bchtest:pmvl5lzvdm6km38lgga64ek5jhdl7e3aqd9895wu04fvhlnare5937w4ywkq57juxsrhvw8ym5d8qx7sz7zz0zvcypqs6kgdsg2g"
+      , "D9FA7C4C6EF56DC4FF423BAAE6D495DBFF663D034A72D1DC7D52CBFE7D1E6858F9D523AC0A7A5C34077638E4DD1A701BD017842789982041")
+    , ( 56
+      , 0
+      , "pref:pmvl5lzvdm6km38lgga64ek5jhdl7e3aqd9895wu04fvhlnare5937w4ywkq57juxsrhvw8ym5d8qx7sz7zz0zvcypqsammyqffl"
+      , "D9FA7C4C6EF56DC4FF423BAAE6D495DBFF663D034A72D1DC7D52CBFE7D1E6858F9D523AC0A7A5C34077638E4DD1A701BD017842789982041")
+    , ( 56
+      , 15
+      , "prefix:0mvl5lzvdm6km38lgga64ek5jhdl7e3aqd9895wu04fvhlnare5937w4ywkq57juxsrhvw8ym5d8qx7sz7zz0zvcypqsgjrqpnw8"
+      , "D9FA7C4C6EF56DC4FF423BAAE6D495DBFF663D034A72D1DC7D52CBFE7D1E6858F9D523AC0A7A5C34077638E4DD1A701BD017842789982041")
+    , ( 64
+      , 0
+      , "bitcoincash:qlg0x333p4238k0qrc5ej7rzfw5g8e4a4r6vvzyrcy8j3s5k0en7calvclhw46hudk5flttj6ydvjc0pv3nchp52amk97tqa5zygg96mtky5sv5w"
+      , "D0F346310D5513D9E01E299978624BA883E6BDA8F4C60883C10F28C2967E67EC77ECC7EEEAEAFC6DA89FAD72D11AC961E164678B868AEEEC5F2C1DA08884175B")
+    , ( 64
+      , 1
+      , "bchtest:plg0x333p4238k0qrc5ej7rzfw5g8e4a4r6vvzyrcy8j3s5k0en7calvclhw46hudk5flttj6ydvjc0pv3nchp52amk97tqa5zygg96mc773cwez"
+      , "D0F346310D5513D9E01E299978624BA883E6BDA8F4C60883C10F28C2967E67EC77ECC7EEEAEAFC6DA89FAD72D11AC961E164678B868AEEEC5F2C1DA08884175B")
+    , ( 64
+      , 0
+      , "pref:plg0x333p4238k0qrc5ej7rzfw5g8e4a4r6vvzyrcy8j3s5k0en7calvclhw46hudk5flttj6ydvjc0pv3nchp52amk97tqa5zygg96mg7pj3lh8"
+      , "D0F346310D5513D9E01E299978624BA883E6BDA8F4C60883C10F28C2967E67EC77ECC7EEEAEAFC6DA89FAD72D11AC961E164678B868AEEEC5F2C1DA08884175B")
+    , ( 64
+      , 15
+      , "prefix:0lg0x333p4238k0qrc5ej7rzfw5g8e4a4r6vvzyrcy8j3s5k0en7calvclhw46hudk5flttj6ydvjc0pv3nchp52amk97tqa5zygg96ms92w6845"
+      , "D0F346310D5513D9E01E299978624BA883E6BDA8F4C60883C10F28C2967E67EC77ECC7EEEAEAFC6DA89FAD72D11AC961E164678B868AEEEC5F2C1DA08884175B")
     ]
-    
